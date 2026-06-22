a05217873aa30b0ec49a304a7de194cb595e194f
Abhishek Gupta <abhishek31052003@gmail.com>
2026-06-22 19:38:32 +0530

    swagger - api docs

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger-output.json" with { type: "json" };
 import { connectDB } from './utils/db.js';
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -16,10 +18,15 @@ await connectDB();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5000",
     credentials: true,
 }))
 app.use("/api/users", userRoutes);
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument)
+);
 app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 5000;
---
+++ server/swagger.js
@@ -0,0 +1,20 @@
+import swaggerAutogen from "swagger-autogen";
+
+const swagger = swaggerAutogen();
+
+const doc = {
+  info: {
+    title: "ChatApp API",
+    description: "API Documentation",
+  },
+  host: " http://localhost:5173",
+  schemes: ["http"],
+};
+
+const outputFile = "./swagger-output.json";
+
+const endpointsFiles = [
+  "./routes/userRoutes.js",
+];
+
+swagger(outputFile, endpointsFiles, doc);
